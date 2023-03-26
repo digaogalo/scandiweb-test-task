91f57426bf17b758025dedac1ed14b4b00fdc67a
digaogalo <digaolimac@gmail.com>
2023-03-26 13:32:24 -0300

    add delete chages

--- resources/views/add.php
+++ resources/views/add.php
@@ -4,7 +4,7 @@ require_once("../../vendor/autoload.php");
 
 use App\models\Book;
 use App\models\DVD;
-use App\models\Furniture; 
+use App\models\Furniture;
 
 if (isset($_POST["SKU"], $_POST["Name"], $_POST["Price"], $_POST["productType"])) {
     $productType = $_POST["productType"];
