a60c82214f43b43c490909dbcbc41efed12aff17
digaogalo <digaolimac@gmail.com>
2023-06-16 11:36:07 -0300

    MVC

--- App/controllers/ProductController.php
+++ App/controllers/ProductController.php
@@ -1,25 +1,28 @@
 <?php
-// ProductController.php
-
-namespace App\controllers;
-
 use App\models\Book;
 use App\models\DVD;
 use App\models\Furniture;
 
-class ProductController
-{
-    public static function getAllProducts()
-    {
+class ProductController {
+    public function getProductList() {
         $allDVD = DVD::getProducts();
         $allBook = Book::getProducts();
         $allFurniture = Furniture::getProducts();
-
         $allProducts = array_merge($allDVD, $allBook, $allFurniture);
-
         array_multisort(array_column($allProducts, 'SKU'), SORT_ASC, SORT_NATURAL | SORT_FLAG_CASE, $allProducts);
 
-        return $allProducts;
+        $results = "";
+        foreach ($allProducts as $row) {
+            $results .= '<div class="card hover-overlay hover-zoom hover-shadow ripple">
+                <input type="checkbox" onchange="check_active()" class="delete-checkbox" name="delete[]" value=' . $row->getSlug("SKU") . '&' . $row->productType . ' id="delete-checkbox">
+                <a href="resources/views/edit-product?SKU=' . $row->getSlug("SKU") . '&type=' . $row->productType . '"></a>
+                <span>' . $row->SKU . '</span>
+                <span>' . $row->Name . '</span>
+                <span>' . $row->Price . ' $</span>
+                <span>' . $row->attributeString() . '</span>
+            </div>';
+        }
+        
+        return $results;
     }
 }
-?>
--- index.php
+++ index.php
@@ -1,58 +1,36 @@
 <?php
 require_once('vendor/autoload.php');
 include("resources/views/partials/head.php");
+include("App/controllers/ProductController.php");
+$controller = new ProductController();
+$results = $controller->getProductList();
 
-use App\controllers\ProductController;
-
-$allProducts = ProductController::getAllProducts();
 ?>
-<html>
+
 <body>
     <div class="container">
-        <div class="title">
-            <h2>Product List</h2>
-            <div>
-              <button   
-                type="button"   
-                class="btn btn-success btn-size"    
-                onclick="window.location.href='resources/views/add-product.php'">   
-                ADD 
-              </button>
-
-                <button     
-                   type="submit"    
-                   class="btn btn-danger btn-size"  
-                   value="delete"   
-                   name="but_delete"    
-                   id="delete-product-btn"  
-                   disabled>    
-                   MASS DELETE  
-                </button>
-                
-            </div>
-        </div>
-        <hr>
         <form method="post" action="resources/views/delete">
+            <div class="title">
+                <h2>Product List</h2>
+                <div>
+                    <button type="button" class="btn btn-success btn-size" onclick="redirectToPage()">
+                        ADD
+                    </button>
+                    <button type="submit" class="btn btn-danger btn-size" value="delete" name="but_delete" id="delete-product-btn" disabled>
+                        MASS DELETE
+                    </button>
+                </div>
+            </div>
+            <hr>
             <div class="row isotope-grid main-grid">
-                <?php foreach ($allProducts as $row): ?>
-                    <div class="card hover-overlay hover-zoom hover-shadow ripple">
-                        <input type="checkbox"  
-                        onchange="check_active()"   
-                        class="delete-checkbox"     
-                        name="delete[]"     
-                        value="<?= $row->getSlug("SKU") ?>&<?= $row->productType ?>"    
-                        id="delete-checkbox">
-
-                        <a href="resources/views/edit-product?SKU=<?= $row->getSlug("SKU") ?>&type=<?= $row->productType ?>"></a>
-                        <span><?= $row->SKU ?></span>
-                        <span><?= $row->Name ?></span>
-                        <span><?= $row->Price ?> $</span>
-                        <span><?= $row->attributeString() ?></span>
-                    </div>
-                <?php endforeach; ?>
+                <?= $results ?>
             </div>
         </form>
     </div>
     <?php include("resources/views/partials/footer.php"); ?>
+    <script>
+        function redirectToPage() {
+            window.location.href = "resources/views/add-product";
+        }
+    </script>
 </body>
-</html>
